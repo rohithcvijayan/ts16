ebb85669631f1b456a852c64232648acd1c5d844
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2020-02-01 09:22:26 +0530

    Interface Discussion finished

---
+++ interface.ts
@@ -0,0 +1,22 @@
+
+// type Points={
+//     maxVal:number,
+//     minVal:number
+// };
+interface Points{
+    maxVal:number,
+    minVal:number
+};
+
+const getDistance = (points: Points): number=>{
+    return points.maxVal - points.minVal;
+};
+
+
+let points:Points={
+    maxVal:54,
+    minVal:35
+};
+let result = getDistance(points);
+// result = getDistance(54, 34);
+console.log(result);
